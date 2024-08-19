fa30f886cb28a77f22a54cedd834acf753b359ec
nikitazapekin <wotblitz362@mail.ru>
2024-08-19 23:22:11 +0300

    first commit

---
+++ graphiql-app/src/app/homepage/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from 'antd';
+
+const Homepage = () => {
+    return (<>
+        Homepage
+        <Button type="primary">Button</Button>
+    </>);
+}
+
+export default Homepage;
---
+++ graphiql-app/src/components/Footer/consts.ts
@@ -0,0 +1,5 @@
+export const githubs = [
+    {to: "https://github.com/suzik86", username: "suzik86"},
+    {to: "https://github.com/geekedger", username: "geekedger"},
+    {to: "https://github.com/nikitazapekin", username: "nikitazapekin"}
+]
