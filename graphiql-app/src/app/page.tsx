a2a721a7713cb38ce34996a5973aadd97629cc81
nikitazapekin <wotblitz362@mail.ru>
2024-08-23 14:48:45 +0300

    remove code smell

--- graphiql-app/src/app/page.tsx
+++ graphiql-app/src/app/page.tsx
@@ -1,14 +1,13 @@
 
 
 import WelcomeBackSection from "../components/WelcomeBackSection/WelcomeBackSection";
-import ModalComponent from "../components/Modal/Modal";
+ 
 export default function Home() {
   return (
     <>
 
 
       <WelcomeBackSection />
-      <ModalComponent />
     </>
   );
 };
