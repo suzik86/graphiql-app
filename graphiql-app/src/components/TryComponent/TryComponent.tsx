5a3c60c30769d635d047272935f3500cac72d5b6
nikitazapekin <wotblitz362@mail.ru>
2024-09-04 21:31:04 +0300

    refactor of code smell

--- graphiql-app/src/components/TryComponent/TryComponent.tsx
+++ graphiql-app/src/components/TryComponent/TryComponent.tsx
@@ -19,36 +19,7 @@ const TryComponent = () => {
           rotate={item.rotate}
         />
       ))}
-      {/*
-            <Button text={"POST"} color={"#49CC90"} top={"50px"}
-            left={"0px"}
-                right={null}
-                bottom={null}
-                rotate={-13}
-
-            />
-            <Button text={"DELETE"} color={"#F93E3E"} top={"150px"}
-                left={null}
-                right={"0"}
-                bottom={null}
-                rotate={13}
-
-            />
-            <Button text={"PUT"} color={"#FCA130"} top={null}
-                left={"0"}
-                right={null}
-                bottom={"150px"}
-                rotate={13}
-
-            />
-            <Button text={"GET"} color={"#61AFFE"} top={null}
-                left={null}
-                right={"0"}
-                bottom={"75px"}
-                rotate={-13}
-                
-                />
-                */}
+     
     </section>
   );
 };
