10927fec6d375a69d981188f94f3dc447f8d3f3b
Irina Shishkova <irina.suzanovich@gmail.com>
2024-09-05 12:09:26 +0300

    fix: add keys

--- graphiql-app/src/components/AboutUsComponent/AboutUsComponent.tsx
+++ graphiql-app/src/components/AboutUsComponent/AboutUsComponent.tsx
@@ -1,8 +1,9 @@
-import styles from "./AboutUsComponent.module.scss";
-import Avatar from "../../assets/rss-logo.svg";
-import { members } from "./consts";
-import MemberCard from "../MemberCard/MemberCard";
 import { useTranslations } from "next-intl";
+import Avatar from "../../assets/rss-logo.svg";
+import MemberCard from "../MemberCard/MemberCard";
+import styles from "./AboutUsComponent.module.scss";
+import { members } from "./consts";
+
 const AboutUsComponent = () => {
   const t = useTranslations("HomePage");
   return (
@@ -10,8 +11,9 @@ const AboutUsComponent = () => {
       <div className={styles.info__inner}>
         <h2 className={styles.info__title}>{t("about-us")}</h2>
         <div className={styles.info__images}>
-          {members.map((item) => (
+          {members.map((item, index) => (
             <MemberCard
+              key={index}
               name={item.name}
               describtion={item.describtion}
               logo={Avatar}
