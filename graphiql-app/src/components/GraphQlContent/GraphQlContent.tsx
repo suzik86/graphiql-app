c50fdf0dda4fddeb4b4d319f811aa22c417503d8
nikitazapekin <wotblitz362@mail.ru>
2024-09-08 11:16:22 +0300

    fix url issues

--- graphiql-app/src/components/GraphQlContent/GraphQlContent.tsx
+++ graphiql-app/src/components/GraphQlContent/GraphQlContent.tsx
@@ -11,7 +11,8 @@ import QueryEditor from "./QueryEditor";
 import SdlEditor from "./SdlEditor";
 import RequestHandlerSdl from "./RequestHandlerSdl";
 import { decodeBase64 } from "../../utils/base64";
-import GraphQLBeautifier from "./Beautifier";
+import { useTranslations } from "next-intl";
+
 export interface Header {
   key: string;
   value: string;
@@ -34,11 +35,214 @@ const getHeadersFromParams = (searchParams: URLSearchParams): Header[] => {
 };
 
 const GrafQlContent = () => {
+  const t = useTranslations("Footer");
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { method } = useParams<{ method: string }>();
-  
-  // Memoized initial state from URL
+
+  const initialState = useMemo(() => {
+    const segments = pathname.split("/");
+    const bodyBase64String = segments.pop();
+    const endpointBase64String = segments.pop();
+
+    let headers: Header[] = [];
+    let currentEndpoint = "";
+    let variables: Variable[] = [];
+    let schema: object | string | null = null;
+
+    if (endpointBase64String && bodyBase64String) {
+      try {
+        const decodedUrl = decodeBase64(endpointBase64String);
+        const decodedBody = decodeBase64(bodyBase64String);
+        const bodyJson = JSON.parse(decodedBody);
+
+        currentEndpoint = decodedUrl;
+        variables = bodyJson.variables || [];
+
+        if (bodyJson.body && bodyJson.body.schema) {
+          schema = bodyJson.body.schema;
+        }
+        headers = getHeadersFromParams(searchParams);
+      } catch (e) {
+        console.error("Error decoding body or endpoint:", e);
+      }
+    }
+
+    console.log("Initial state:", { headers, currentEndpoint, variables, schema });
+    return { headers, currentEndpoint, variables, schema };
+  }, [pathname, searchParams]);
+
+  const [headers, setHeaders] = useState<Header[]>(initialState.headers);
+  const [currentEndpoint, setEndpoint] = useState<string>(initialState.currentEndpoint);
+  const [variables, setVariables] = useState<Variable[]>(initialState.variables);
+  const [schema, setSchema] = useState<object | string | null>(initialState.schema);
+  const [currentMethod, setMethod] = useState<string>(method || "query");
+  const [currentBody, setBody] = useState<object | string | null>(null);
+  const [isVariablesVisible, setIsVariablesVisible] = useState<boolean>(true);
+  const [editorMode, setEditorMode] = useState<"json" | "text">("json");
+
+  const requestHandlerRef = useRef<React.ElementRef<typeof RequestHandler>>(null);
+  const requestHandlerSdlRef = useRef<React.ElementRef<typeof RequestHandlerSdl>>(null);
+  const [sdlEndpoint, setSdlEndpoint] = useState<string>("");
+
+  const sendRequest = useCallback(() => {
+    requestHandlerRef.current?.sendRequest();
+  }, []);
+
+  const sendRequestSdl = useCallback((endpoint: string) => {
+    requestHandlerSdlRef.current?.sendRequest(endpoint);
+  }, []);
+
+  useEffect(() => {
+    if (schema) {
+      setBody((prev) => {
+        if (typeof prev === "object" && prev !== null) {
+          return {
+            ...prev,
+            schema: schema,
+          };
+        } else {
+          return { schema: schema };
+        }
+      });
+    }
+  }, [schema]);
+
+  useEffect(() => {
+    updateURL("graphql", currentEndpoint, currentBody, headers, variables);
+  }, [currentEndpoint, headers, variables, currentBody, schema]);
+
+ 
+  useEffect(() => {
+    console.log("Schema after useEffect:", schema);
+  }, [schema]);
+
+  return (
+    <section className={styles.content}>
+      <div className={styles.content__inner}>
+        <div className={styles.content__wrapper}>
+          <h1 className={styles.content__title}>GraphQl Client</h1>
+          <div className={styles.content__background} />
+          <div className={styles.content__inputs}>
+            <UrlEditor
+              currentMethod={currentMethod}
+              setMethod={setMethod}
+              currentEndpoint={currentEndpoint}
+              setEndpoint={setEndpoint}
+              onSendRequest={sendRequest}
+            />
+            <SdlEditor
+              currentMethod={currentMethod}
+              setMethod={setMethod}
+              currentEndpoint={sdlEndpoint}
+              setEndpoint={setSdlEndpoint}
+              onSendRequest={sendRequestSdl}
+            />
+          </div>
+          <HeaderEditor
+            title={"Headers"}
+            method={currentMethod}
+            endpoint={currentEndpoint}
+            body={currentBody}
+            headers={headers}
+            setHeaders={setHeaders}
+            variables={variables}
+          />
+          <div
+            className={styles.content__toggle}
+            onClick={() => setIsVariablesVisible(!isVariablesVisible)}
+          >
+            Variables {isVariablesVisible ? "-" : "+"}
+          </div>
+          {isVariablesVisible && (
+            <VariableEditor
+              variables={variables}
+              setVariables={setVariables}
+              body={currentBody as string}
+              onUpdateBody={setBody}
+            />
+          )}
+          <QueryEditor
+            body={schema}
+            title={"Request editor"}
+            variables={variables}
+            editorMode={"graphql"}
+            setEditorMode={setEditorMode}
+            handleChangeSchema={setSchema}
+            method={currentMethod}
+            schema={String(schema)}
+          />
+          <RequestHandler
+            schema={String(schema)}
+            method={currentMethod}
+            endpoint={currentEndpoint}
+            headers={headers}
+            body={currentBody}
+            editorMode={editorMode}
+            variables={variables}
+            ref={requestHandlerRef}
+          />
+          <RequestHandlerSdl
+            schema={String(schema)}
+            method={currentMethod}
+            endpoint={sdlEndpoint}
+            headers={headers}
+            body={currentBody}
+            editorMode={editorMode}
+            variables={variables}
+            ref={requestHandlerSdlRef}
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default GrafQlContent;
+
+/*
+"use client";
+import UrlEditor from "./UrlEditor";
+import { useState, useEffect, useRef, useCallback, useMemo } from "react";
+import { updateURL } from "../../utils/urlUpdater";
+import styles from "./GraphQlContent.module.scss";
+import VariableEditor from "./VariablesEditor";
+import HeaderEditor from "./HeaderEditor";
+import RequestHandler from "./RequestHandler";
+import { useParams, useSearchParams, usePathname } from "next/navigation";
+import QueryEditor from "./QueryEditor";
+import SdlEditor from "./SdlEditor";
+import RequestHandlerSdl from "./RequestHandlerSdl";
+import { decodeBase64 } from "../../utils/base64";
+import { useTranslations } from "next-intl";
+export interface Header {
+  key: string;
+  value: string;
+  included: boolean;
+}
+
+export interface Variable {
+  key: string;
+  value: string;
+  included: boolean;
+}
+
+const getHeadersFromParams = (searchParams: URLSearchParams): Header[] => {
+  return Array.from(searchParams.entries()).reduce<Header[]>((acc, [key, value]) => {
+    if (key !== "method" && key !== "encodedEndpoint" && key !== "encodedData") {
+      acc.push({ key, value: decodeURIComponent(value), included: true });
+    }
+    return acc;
+  }, []);
+};
+
+const GrafQlContent = () => {
+
+  const t = useTranslations("Footer");
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { method } = useParams<{ method: string }>();
+ 
   const initialState = useMemo(() => {
     const segments = pathname.split("/");
     const bodyBase64String = segments.pop();
@@ -108,8 +312,8 @@ const GrafQlContent = () => {
 
   useEffect(() => {
     updateURL("graphql", currentEndpoint, currentBody, headers, variables);
-  }, [currentEndpoint, headers, variables, currentBody]);
-
+  }, [currentEndpoint, headers, variables, currentBody, schema]);
+//{t("message")}
   return (
     <section className={styles.content}>
       <div className={styles.content__inner}>
@@ -189,13 +393,13 @@ const GrafQlContent = () => {
         </div>
       </div>
 
-      <GraphQLBeautifier />
+     
     </section>
   );
 };
 
 export default GrafQlContent;
-
+*/
 
 
 
--- graphiql-app/src/components/GraphQlContent/QueryEditor.tsx
+++ graphiql-app/src/components/GraphQlContent/QueryEditor.tsx
@@ -1,4 +1,204 @@
 
+import React, { useEffect, useRef, useState } from "react";
+import { Editor, type Monaco } from "@monaco-editor/react";
+import * as monaco from "monaco-editor";
+import styles from "./RequestBodyEditor.module.scss";
+import { parse, print } from 'graphql';
+
+const myCustomTheme: monaco.editor.IStandaloneThemeData = {
+  base: "vs-dark",
+  inherit: true,
+  rules: [
+    { token: "comment", foreground: "7d8799", fontStyle: "italic" },
+    { token: "keyword", foreground: "c678dd" },
+    { token: "identifier", foreground: "61afef" },
+    { token: "string", foreground: "98c379" },
+    { token: "number", foreground: "d19a66" },
+    { token: "type", foreground: "e5c07b" },
+    { token: "function", foreground: "61afef" },
+    { token: "operator", foreground: "56b6c2" },
+    { token: "variable", foreground: "e06c75" },
+    { token: "invalid", foreground: "ffffff", background: "e06c75" },
+  ],
+  colors: {
+    "editor.foreground": "#abb2bf",
+    "editor.background": "#6699ff0b",
+    "editorCursor.foreground": "#528bff",
+    "editorLineNumber.foreground": "#7d8799",
+    "editor.inactiveSelectionBackground": "#3E4451",
+    "editor.selectionHighlightBackground": "#aafe661a",
+    "editor.findMatchHighlightBackground": "#72a1ff59",
+    "editor.findMatchBackground": "#6199ff2f",
+  },
+};
+
+type Variable = {
+  key: string;
+  value: string;
+  included: boolean;
+};
+
+type RequestBodyEditorProps = {
+  title: string;
+  body: object | string | null;
+  setBlurredBody?: React.Dispatch<React.SetStateAction<string>>;
+  variables?: Variable[];
+  editorMode: "graphql" | "json" | "text";
+  setEditorMode?: (mode: "json" | "text") => void;
+  readOnly?: boolean;
+  handleChangeSchema: (schema: string) => void;
+  method: string;
+  schema: string;
+};
+
+const QueryEditor: React.FC<RequestBodyEditorProps> = ({
+  title,
+  setBlurredBody = () => { },
+  editorMode,
+  readOnly = false,
+  handleChangeSchema,
+  method,
+  schema, // добавил schema в параметры
+}) => {
+  const editorRef = useRef<monaco.editor.IStandaloneCodeEditor | null>(null);
+  const [editorHeight, setEditorHeight] = useState<number>(200);
+  const [editorValue, setEditorValue] = useState(schema || ""); // инициализация из schema
+  const maxHeight = 500;
+
+  useEffect(() => {
+    setEditorValue(`${method} {\n\n}`);
+  }, [method]);
+
+  // Следим за изменением schema и обновляем editorValue
+  useEffect(() => {
+    setEditorValue(schema || "");
+    if (editorRef.current) {
+      editorRef.current.setValue(schema || "");
+    }
+  }, [schema]);
+
+  const handleEditorDidMount = (
+    editor: monaco.editor.IStandaloneCodeEditor
+  ) => {
+    editorRef.current = editor;
+
+    if (readOnly) {
+      editor.updateOptions({ readOnly: true });
+    }
+
+    editor.onDidBlurEditorWidget(() => {
+      if (!readOnly) {
+        const latestBody = editorRef.current?.getValue() || "";
+        setBlurredBody(latestBody);
+      }
+    });
+
+    editor.onDidContentSizeChange(() => {
+      const contentHeight = editor.getContentHeight();
+      const newHeight = Math.min(Math.max(contentHeight, 200), maxHeight);
+      setEditorHeight(newHeight);
+    });
+  };
+
+  const handleEditorTheme = (monaco: Monaco) => {
+    monaco.editor.defineTheme("myCustomTheme", {
+      ...myCustomTheme,
+    });
+    monaco.editor.setTheme("myCustomTheme");
+    monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
+      tsx: "react",
+    });
+  };
+/*
+  const handleBeautify = () => {
+    try {
+      if (editorRef.current) {
+        const currentSchema = editorRef.current.getValue();
+        const ast = parse(currentSchema);
+        const formatted = print(ast);
+        handleChangeSchema(formatted);
+        editorRef.current.setValue(formatted);
+      }
+    } catch (error) {
+      console.error('Error formatting code:', error);
+      handleChangeSchema("Something went wrong...");
+    }
+  };
+*/
+const handleBeautify = () => {
+  try {
+    if (editorRef.current) {
+      const currentSchema = editorRef.current.getValue();
+      let formatted = currentSchema;
+
+      // Парсинг и форматирование
+      const ast = parse(currentSchema);
+      formatted = print(ast);
+
+      // Если ключевое слово `query` отсутствует и тело запроса начинается с {, добавляем его
+      if (!formatted.startsWith('query') && formatted.trim().startsWith('{')) {
+        formatted = `query ${formatted}`;
+      }
+
+      handleChangeSchema(formatted);
+      editorRef.current.setValue(formatted);
+    }
+  } catch (error) {
+    console.error('Error formatting code:', error);
+    handleChangeSchema("Something went wrong...");
+  }
+};
+
+  const handleEditorChange = (value: string | undefined) => {
+    setEditorValue(value || "");
+    handleChangeSchema(value || "");
+  };
+
+  return (
+    <div className={styles.body}>
+      <p className={styles.body__title}>{title}</p>
+      <div className={styles.body__container}>
+        {!readOnly && (
+          <div className={styles.body__controls}>
+            {editorMode === "graphql" && (
+              <span className={styles.body__beautify} onClick={handleBeautify}>
+                Beautify
+              </span>
+            )}
+          </div>
+        )}
+        <Editor
+          height={editorHeight}
+          language="graphql"
+          theme="myCustomTheme"
+          loading="Loading..."
+          value={editorValue}
+          onMount={handleEditorDidMount}
+          beforeMount={handleEditorTheme}
+          options={{
+            fixedOverflowWidgets: true,
+            placeholder: "Enter your GraphQL query...",
+            minimap: { enabled: false },
+            wordWrap: "on",
+            automaticLayout: true,
+            scrollBeyondLastLine: false,
+            lineNumbers: "on",
+            fontFamily: "Nunito",
+            readOnly,
+            formatOnType: !readOnly,
+            formatOnPaste: readOnly,
+            fontSize: 22,
+          }}
+          onChange={handleEditorChange}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default QueryEditor;
+
+/*
 
 import React, { useEffect, useRef, useState } from "react";
 import { Editor, type Monaco } from "@monaco-editor/react";
@@ -54,14 +254,11 @@ type RequestBodyEditorProps = {
 
 const QueryEditor: React.FC<RequestBodyEditorProps> = ({
   title,
-  body,
   setBlurredBody = () => { },
   editorMode,
-  setEditorMode = () => { },
   readOnly = false,
   handleChangeSchema,
   method,
-  schema,
 }) => {
   const editorRef = useRef<monaco.editor.IStandaloneCodeEditor | null>(null);
   const [editorHeight, setEditorHeight] = useState<number>(200);
@@ -116,6 +313,7 @@ const QueryEditor: React.FC<RequestBodyEditorProps> = ({
       }
     } catch (error) {
       console.error('Error formatting code:', error);
+      handleChangeSchema("Something went wrong...");  
     }
   };
   
@@ -170,4 +368,4 @@ const QueryEditor: React.FC<RequestBodyEditorProps> = ({
 
 export default QueryEditor;
 
- 
+ */
