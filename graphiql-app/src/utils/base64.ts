23dd9bd5a2ec1bf6f9ba8f976c4b62c7b9dbc6fd
nikitazapekin <wotblitz362@mail.ru>
2024-09-11 14:06:48 +0300

    refactor of incorrect unicode symbols in url

--- graphiql-app/src/utils/base64.ts
+++ graphiql-app/src/utils/base64.ts
@@ -1,39 +1,45 @@
 export function encodeBase64(str: string): string {
   if (typeof window !== "undefined") {
-    // Client-side environment
     return btoa(unescape(encodeURIComponent(str)));
   } else {
-    // Server-side environment
+   
     return Buffer.from(str, "utf-8").toString("base64");
   }
 }
 
-// Function to decode a string from base64// Function to decode a string from Base64
 export function decodeBase64(str: string): string {
   if (typeof window !== "undefined") {
-    // Ensure string is valid Base64
     try {
-      // Decode URL-encoded string first
       const decodedStr = decodeURIComponent(str);
 
-      // Adding padding if missing and decoding Base64
       const paddedStr = decodedStr.padEnd(
         decodedStr.length + ((4 - (decodedStr.length % 4)) % 4),
-        "=",
+        "="
       );
-      return decodeURIComponent(escape(atob(paddedStr)));
+
+   
+      let result = decodeURIComponent(escape(atob(paddedStr)));
+ 
+      result = result.replace(/[\x00-\x1F\x7F-\x9F]/g, "");
+
+      return result;
     } catch (e) {
       console.error("Base64 decoding failed:", e);
       return "";
     }
   } else {
-    // Server-side environment
     try {
-      const decodedStr = decodeURIComponent(str); // Handle URL encoding
-      return Buffer.from(decodedStr, "base64").toString("utf-8");
+      const decodedStr = decodeURIComponent(str);  
+      let result = Buffer.from(decodedStr, "base64").toString("utf-8");
+ 
+      result = result.replace(/[\x00-\x1F\x7F-\x9F]/g, "");
+
+      return result;
     } catch (e) {
       console.error("Base64 decoding failed on server:", e);
       return "";
     }
   }
 }
+
+ 
